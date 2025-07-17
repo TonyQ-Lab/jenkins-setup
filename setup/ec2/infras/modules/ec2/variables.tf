9e178624f74e3d4659b31878dde6a2d365d6835a
TonyQ2k3 <ntbquoc2015@gmail.com>
2025-07-17 16:08:06 +0700

    Added EC2 setup

---
+++ setup/ec2/infras/modules/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "subnet_id" {
+  description = "The ID of the subnet where the EC2 instances will be launched"
+  type        = string
+  default     = ""
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs to associate with the EC2 instances"
+  type        = list(string)
+  default     = []
+}
---
+++ setup/ec2/scripts/install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Pipeline fail
+set -euxo pipefail
+
+# Install Java 21
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install fontconfig openjdk-21-jre openjdk-21-jdk -y
+java -version
+echo "================ Java 21 installed successfully. ================"
+
+# Install Jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+echo "================ Jenkins installed successfully. ================"
+
+# Start Jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+echo "================ Jenkins started successfully. ================"
+
+# Install Docker (for building tasks)
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker jenkins
